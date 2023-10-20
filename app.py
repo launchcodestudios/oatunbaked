b22dd5c37fa593d8f62eb53795238ef863a1f024
Michael Santelia <ecumike@gmail.com>
2023-10-20 14:29:38 -0400

    Page setup (#1)
    * Checkpoint
    
    * Housekeeping: Icon
    
    * Update: Final touches

--- app.py
+++ app.py
@@ -11,7 +11,95 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	data = {
+		'cookies': [
+			{
+				'color': 'oat',
+				'name': 'Vanilla',
+				'descr': 'vanilla and white chocolate cookie with white chocolate drizzle'
+			},
+			{
+				'color': 'mint',
+				'name': 'S\'mores',
+				'descr': 'milk chocolate cookie with marshmallows and grahams'
+			},
+			{
+				'color': 'oat',
+				'name': 'Pumpkin pie',
+				'descr': 'vanilla pumpkin cookie with cinnamon and spice topped with a candy corn'
+			},
+			{
+				'color': 'mint',
+				'name': 'Apple fritter',
+				'descr': 'vanilla cookie with apple chunks,cinnamon and cider glaze'
+			},
+			{
+				'color': 'oat',
+				'name': 'Maple pecan',
+				'descr': 'vanilla maple cookie with chopped pecans and maple glaze'
+			},
+			{
+				'color': 'mint',
+				'name': 'Peanut butter & Nutelly',
+				'descr': 'classic chocolate cookie with peanut butter and nutella'
+			},
+			{
+				'color': 'oat',
+				'name': 'Chocolate toffee',
+				'descr': 'semisweet chocolate cookie with toffee bits and toffee topping'
+			},
+			{
+				'color': 'mint',
+				'name': 'Chocolate coconut',
+				'descr': 'semisweet chocolate cookie with coconut flakes and coconut topping'
+			},
+			{
+				'color': 'oat',
+				'name': 'Hot chocolate',
+				'descr': 'milk chocolate cookie with mini marshmallows'
+			},
+		],
+		'floofs': [
+			{
+				'color': 'armygreen',
+				'name': 'Confetti floof',
+				'descr': 'vanilla cookies with sprinkle filling'
+			},
+			{
+				'color': 'armygreen',
+				'name': 'Lemon floof',
+				'descr': 'vanilla cookies with lemon filling'
+			},
+			{
+				'color': 'armygreen',
+				'name': 'Seasonal floof',
+				'descr': 'seasonal'
+			},
+		],
+		'cookieDoughs': [
+			{
+				'omitRollover': True,
+				'color': 'blue',
+				'name': 'Chocolate chip',
+				'descr': 'TBD'
+			}
+		],
+		'beverages': [
+			{
+				'omitRollover': True,
+				'color': 'pink',
+				'name': 'Apple pie slushie',
+				'descr': 'apple pie filling in apple slushie, topped with strussel crumble'
+			},
+			{
+				'omitRollover': True,
+				'color': 'pink',
+				'name': 'Frozen hot chocolate',
+				'descr': 'chocolate frapé topped with whipped cream & chocolate shavings'
+			}
+		]
+	}
+	return render_template('index.html', data=data)
 	
 
 if __name__ == "__main__":
