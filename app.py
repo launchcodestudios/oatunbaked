82b9430a483595945be55f2088b667f5e7811378
Michael Santelia <ecumike@gmail.com>
2023-10-24 17:23:45 -0400

    Update: Converted to flat file generation. Prepping for SSG setup

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
 import os
+
+import shutil
+from distutils.dir_util import copy_tree
+
 import webbrowser
 from threading import Timer
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 from dotenv import load_dotenv
+from jinja2 import Environment, PackageLoader
 
 from data import menuItems
-
+ 
 load_dotenv()
 
 app = Flask(__name__)
 
 
+def generateFiles():
+	# Blow away build dir and create new clean build.
+	shutil.rmtree('./build')
+	os.makedirs('./build/static')
+	copy_tree('./static', './build/static')
+	
+	# Render index page template and save as file in build dir.
+	env = Environment(loader=PackageLoader('app'))
+	template = env.get_template('index.html')
+ 	
+	root = os.path.dirname(os.path.abspath(__file__))
+	filename = os.path.join(root, 'build', 'index.html')
+ 	
+	with open(filename, 'w') as fh:
+		fh.write(template.render(data=menuItems))
+ 	
+ 	
 @app.route('/')
 def home():
-	return render_template('index.html', data=menuItems)
+	generateFiles()
+	
+	return send_from_directory('build', 'index.html')
 	
 
 if __name__ == "__main__":
