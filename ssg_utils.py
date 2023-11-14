35488eb9420ea76ba98bbb48907b94167d9c2129
Michael Santelia <ecumike@gmail.com>
2023-11-14 12:02:44 -0500

    New: Contact form

--- ssg_utils.py
+++ ssg_utils.py
@@ -11,7 +11,7 @@ def generateFile(ENV, file, data):
 	# Copy static files to build, render page template and save as file in build dir.
 	copy_tree('./static', f'{BUILD_DIR}/static')
 	template = ENV.get_template(file)
-	renderedHtml = template.render(data=data)
+	renderedHtml = template.render(data=data).replace('\n','').replace('\t','')
  	
 	filename = os.path.join(BUILD_DIR, file)
 	with open(filename, 'w') as fh:
