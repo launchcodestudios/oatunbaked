dec1cd9d10beef431d1a1071d6c5c75742dcc1d2
Michael Santelia <ecumike@gmail.com>
2025-05-17 22:48:37 -0400

    Housekeeping

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 ENV = Environment(loader=PackageLoader('app'))
 
 # NOTE: You (currently) have to view a page to render the flat file for it.
-# Everything below is only used for generating the HTML files and viewing on dev. 
+# Everything below is only used for generating the HTML files and viewing on dev.
 # Prod is served as flat files from /build/.
 
 @app.route('/')
@@ -25,7 +25,7 @@ def home():
 	fileName = 'index.html'
 	generateFile(ENV, fileName, menuItems)
 	return send_from_directory('build', fileName)
-	
+
 @app.route('/sitemap.txt')
 def sitemap():
 	fileName = 'sitemap.txt'
@@ -34,19 +34,18 @@ def sitemap():
 
 @app.route('/robots.txt')
 def robots():
-		fileName = 'robots.txt'
-		generateFile(ENV, fileName, {})
-		return send_from_directory('build', fileName)
-	
+	fileName = 'robots.txt'
+	generateFile(ENV, fileName, {})
+	return send_from_directory('build', fileName)
 
-	
+
 # Only used for dev. Prod is served as flat files from /build/.
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
-	
+
 	def open_browser():
 		if not os.environ.get("WERKZEUG_RUN_MAIN"):
 			webbrowser.open_new(f'http://127.0.0.1:{ port }')
-		
+
 	Timer(.5, open_browser).start()
 	app.run(host='0.0.0.0', port=port, debug=True)
